0eae09088dba125d926977bcc814960d32fadb3d
inkuksuss <inkuksuss@naver.com>
2023-10-27 18:00:27 +0900

    오류 수정

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import React from "react";
 import MainPage from "./components/common/MainPage";
 
 const App = () => {
-  return (
-    <div className="app">
-      <MainPage />
-    </div>
-  );
+	return (
+		<div className="app">
+			<MainPage />
+		</div>
+	);
 };
 
 export default App;
