5d48a61c3b74ce0bbaa843347bb2f2d0ed79fb56
hisungmi <rosa6529@naver.com>
2023-10-29 16:09:37 +0900

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,52 +1,35 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-	},
-	extends: [
-		"eslint:recommended",
-		"plugin:react/recommended",
-		"plugin:jsx-a11y/recommended",
-		"plugin:prettier/recommended",
-		"plugin:react-hooks/recommended",
-		"plugin:@typescript-eslint/eslint-recommended",
-		"plugin:@typescript-eslint/recommended",
-		"react-app",
-		"react-app/jest",
-		"prettier"
-	],
-	overrides: [
-		{
-			env: {
-				node: true,
-			},
-			files: [".eslintrc.{js,cjs}"],
-			parserOptions: {
-				sourceType: "script",
-			},
-		},
-	],
-	parserOptions: {
-		ecmaFeatures: {
-			"jsx": true
-		},
-		ecmaVersion: "latest",
-		sourceType: "module",
-	},
-	plugins: ["react", "@typescript-eslint",'react-hooks', 'jsx-a11y'],
-	rules: {
-		"prettier/prettier": [
-			"error",
-			{
-				useTabs: false,
-				endOfLine: 'auto',
-			}
-		],
-		'react/react-in-jsx-scope': 'off',
-	},
-	settings: {
-		"react": {
-			"version": "detect"
-		}
-	}
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:jsx-a11y/recommended",
+        "plugin:prettier/recommended",
+        "plugin:react-hooks/recommended",
+    ],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: [".eslintrc.{js,cjs}"],
+            parserOptions: {
+                sourceType: "script",
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["react"],
+    rules: {},
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
 };
--- src/components/common/MainPage.js
+++ src/components/common/MainPage.js
@@ -5,36 +5,33 @@ import { decrement, increment } from "../../app/testSlice";
 import Select from "react-select";
 
 const options = [
-	{ value: "chocolate", label: "Chocolate" },
-	{ value: "strawberry", label: "Strawberry" },
-	{ value: "vanilla", label: "Vanilla" },
+    { value: "chocolate", label: "Chocolate" },
+    { value: "strawberry", label: "Strawberry" },
+    { value: "vanilla", label: "Vanilla" },
 ];
 
 const MainPage = () => {
-	const count = useSelector((state) => state.counter.value);
-	const navigate = useNavigate();
-	const dispatch = useDispatch();
-	const [selectedOption, handleSelectChange] = useState();
-	return (
-		<div>
-			<div>
-				<Select
-					value={selectedOption}
-					onChange={handleSelectChange}
-					options={options}
-				/>
-			</div>
-			<div>
-				<button onClick={() => dispatch(decrement())}>1씩 감소</button>
-				<span>{count}</span>
-				<button onClick={() => dispatch(increment())}>1씩 증가</button>
-			</div>
-			<div onClick={() => navigate("/myprofile")}>
-				my profile page
-			</div>
-
-		</div>
-	);
+    const count = useSelector((state) => state.counter.value);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const [selectedOption, handleSelectChange] = useState();
+    return (
+        <div>
+            <div>
+                <Select
+                    value={selectedOption}
+                    onChange={handleSelectChange}
+                    options={options}
+                />
+            </div>
+            <div>
+                <button onClick={() => dispatch(decrement())}>1씩 감소</button>
+                <span>{count}</span>
+                <button onClick={() => dispatch(increment())}>1씩 증가</button>
+            </div>
+            <div onClick={() => navigate("/myprofile")}>my profile page</div>
+        </div>
+    );
 };
 
 export default MainPage;
